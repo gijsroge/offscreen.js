2fb3420cd1ad21310da82c4f3af309dcc347afe7
Gijs Rogé <rogegijs@gmail.com>
2014-02-11 18:45:30 +0100

    Added a left scenario
    Added a left side off screen detection.

--- offscreen.js
+++ offscreen.js
@@ -2,7 +2,8 @@ $.fn.offscreen = function(options) {
 
     //set default options
     var defaults = {
-        useClass: 'offviewport',
+        rightClass: 'right-edge',
+        leftClass: 'left-edge',
         smartResize: false
     };
 
@@ -14,9 +15,17 @@ $.fn.offscreen = function(options) {
         var windowWidth = $(window).width();
         $($this).each(function(){
             if(windowWidth < ($(this).offset().left + $(this).outerWidth())){
-                $(this).addClass(defaults.useClass);
+                $(this).addClass(defaults.rightClass);
             }else if(windowWidth > ($(this).offset().left + $(this).outerWidth() * 1.5)){
-                $(this).removeClass(defaults.useClass);
+                $(this).removeClass(defaults.rightClass);
+            }
+
+            if(($(this).offset().left < 0)){
+                $(this).addClass(defaults.leftClass);
+                console.log('fall of left');
+            }else if(($(this).offset().left > 0)){
+                $(this).removeClass(defaults.leftClass);
+                console.log('remove left');
             }
         });
     };
@@ -25,7 +34,7 @@ $.fn.offscreen = function(options) {
     offscreenInitiate();
 
     if(defaults.smartResize === true){
-      
+
       // debouncing function from John Hann
       // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
       (function(e,t){var n=function(e,t,n){var r;return function(){function u(){if(!n)e.apply(s,o);r=null}var s=this,o=arguments;if(r)clearTimeout(r);else if(n)e.apply(s,o);r=setTimeout(u,t||100)}};jQuery.fn[t]=function(e){return e?this.bind("resize",n(e)):this.trigger(t)}})(jQuery,"smartresize");
