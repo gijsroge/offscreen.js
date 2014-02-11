76fd11541b254e021832fef4b21c6d0f7302e25b
Gijs Rogé <rogegijs@gmail.com>
2014-02-11 18:26:14 +0100

    added demo and custom options
    -abillity to set own classname that will be added once the item falls of
    the screen.
    -added smartresize option for performance gain (aka debounced resize)
    -added a demo to show off the functionality

--- README.md
+++ README.md
@@ -4,7 +4,13 @@ offscreen.js
 Detects if an element falls of the right side of your screen
 
 #### What does it do?
-Adds a class "offscreen" to all your elements with the defined selector that fall of the right side of the screen.
+Adds a class "offscreen" to all your elements with the defined selector that fall of the right side of the screen. 
+
+<dl>
+  <dd>checks all the elements with the defined selector.</dd>
+  <dt>rechecks everything when resize (optimized resize)</dt>
+</dl>
+
 
 #### Usage:
 ```
--- offscreen.js
+++ offscreen.js
@@ -1,12 +1,22 @@
-$.fn.offscreen = function() {
+$.fn.offscreen = function(options) {
+
+    //set default options
+    var defaults = {
+        useClass: 'offviewport',
+        smartResize: false
+    };
+
+    //call in the default otions
+    var options = $.extend(defaults, options);
+
     var $this = $(this);
     function offscreenInitiate(){
         var windowWidth = $(window).width();
         $($this).each(function(){
             if(windowWidth < ($(this).offset().left + $(this).outerWidth())){
-                $(this).addClass('offviewport');
-            }else if(windowWidth > ($(this).offset().left + $(this).outerWidth() * 2)){
-                $(this).removeClass('offviewport');
+                $(this).addClass(defaults.useClass);
+            }else if(windowWidth > ($(this).offset().left + $(this).outerWidth() * 1.5)){
+                $(this).removeClass(defaults.useClass);
             }
         });
     };
@@ -14,7 +24,20 @@ $.fn.offscreen = function() {
     //get the ball rolling
     offscreenInitiate();
 
-    $(window).resize(function(){
+    if(defaults.smartResize === true){
+      
+      // debouncing function from John Hann
+      // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
+      (function(e,t){var n=function(e,t,n){var r;return function(){function u(){if(!n)e.apply(s,o);r=null}var s=this,o=arguments;if(r)clearTimeout(r);else if(n)e.apply(s,o);r=setTimeout(u,t||100)}};jQuery.fn[t]=function(e){return e?this.bind("resize",n(e)):this.trigger(t)}})(jQuery,"smartresize");
+      
+      $(window).smartresize(function(){
+        console.log('smart resize');
         offscreenInitiate();
-    });
+      });
+    }else{
+      $( window ).resize(function() {
+        console.log('resize');
+        offscreenInitiate();
+      });
+    }
 }
