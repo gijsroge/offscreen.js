5fd1597610b8827ed1f0118c47d43d40b370a820
Gijs Roge <gijs.roge@brightsites.be>
2014-02-12 11:04:09 +0100

    rollback

--- offscreen.js
+++ offscreen.js
@@ -1,50 +1,53 @@
-$.fn.offscreen = function(options) {
+(function ( $ ) { 
 
-    //set default options
-    var defaults = {
-        rightClass: 'right-edge',
-        leftClass: 'left-edge',
-        smartResize: false
-    };
+    $.fn.offscreen = function(options) {
+        
+        //set default options
+        var defaults = {
+            rightClass: 'right-edge',
+            leftClass: 'left-edge',
+            smartResize: false
+        };
 
-    //call in the default otions
-    var options = $.extend(defaults, options);
+        //call in the default otions
+        var options = $.extend(defaults, options);
 
-    var $this = $(this);
-    function offscreenInitiate(){
-        var windowWidth = $(window).width();
-        $($this).each(function(){
-            if(windowWidth < ($(this).offset().left + $(this).outerWidth())){
-                $(this).addClass(defaults.rightClass);
-            }else if(windowWidth > ($(this).offset().left + $(this).outerWidth() * 1.5)){
-                $(this).removeClass(defaults.rightClass);
-            }
+        var $this = $(this);
+        function offscreenInitiate(){
+            var windowWidth = $(window).width();
+            $($this).each(function(){
+                if(windowWidth < ($(this).offset().left + $(this).outerWidth())){
+                    $(this).addClass(defaults.rightClass);
+                }else if(windowWidth > ($(this).offset().left + $(this).outerWidth() * 1.5)){
+                    $(this).removeClass(defaults.rightClass);
+                }
 
-            if(($(this).offset().left < 0)){
-                console.log('added');
-                $(this).addClass(defaults.leftClass);
-            }else if(($(this).offset().left * 2 > $(this).outerWidth())){
-                console.log('removed');
-                $(this).removeClass(defaults.leftClass);
-            }
-        });
-    };
+                if(($(this).offset().left < 0)){
+                    console.log('added');
+                    $(this).addClass(defaults.leftClass);
+                }else if(($(this).offset().left * 2 > $(this).outerWidth())){
+                    console.log('removed');
+                    $(this).removeClass(defaults.leftClass);
+                }
+            });
+        };
 
-    //get the ball rolling
-    offscreenInitiate();
-
-    if(defaults.smartResize === true){
-
-      // debouncing function from John Hann
-      // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
-      (function(e,t){var n=function(e,t,n){var r;return function(){function u(){if(!n)e.apply(s,o);r=null}var s=this,o=arguments;if(r)clearTimeout(r);else if(n)e.apply(s,o);r=setTimeout(u,t||100)}};jQuery.fn[t]=function(e){return e?this.bind("resize",n(e)):this.trigger(t)}})(jQuery,"smartresize");
-      
-      $(window).smartresize(function(){
+        //get the ball rolling
         offscreenInitiate();
-      });
-    }else{
-      $( window ).resize(function() {
-        offscreenInitiate();
-      });
+
+        if(defaults.smartResize === true){
+
+          // debouncing function from John Hann
+          // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
+          (function(e,t){var n=function(e,t,n){var r;return function(){function u(){if(!n)e.apply(s,o);r=null}var s=this,o=arguments;if(r)clearTimeout(r);else if(n)e.apply(s,o);r=setTimeout(u,t||100)}};jQuery.fn[t]=function(e){return e?this.bind("resize",n(e)):this.trigger(t)}})(jQuery,"smartresize");
+          
+          $(window).smartresize(function(){
+            offscreenInitiate();
+          });
+        }else{
+          $( window ).resize(function() {
+            offscreenInitiate();
+          });
+        }
     }
-}
+}( jQuery ));
